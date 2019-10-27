702a5fcd5b013b57a2bfd3167af66e71913d0b28
Anthony Tamburro <anthonytamburro89@gmail.com>
2019-10-27 10:19:13 -0400

    check and delete buttons working with event delegation process

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-$(function() {
+$(function() {/*
  $('.shopping-item-toggle').click(function(){
     $(this).closest('.shopping-item').toggleClass('shopping-item__checked');
     alert('script loaded');
- })
+ })*/
 
+ $('ul').on('click', '.shopping-item-toggle', function() {
+  $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+});
+
+$('ul').on('click', '.shopping-item-delete', function(){
+  $(this).closest('li').remove();
+});
+/*
     $('.shopping-item-delete').click(function() {
      $(this).parent(this).parent().remove();
      alert('clicked delete');
-  })
+  });
  
   $('.js-shopping-list-form').submit(function() {
     $(this).preventDefault();
@@ -27,8 +35,8 @@ $(function() {
     </div> 
     "</li>"
     );
-    })
-    })
+    });
+    })*/
   })
 
  /*
