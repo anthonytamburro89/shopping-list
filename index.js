9ba2664b5a1805a1afceb244d1d4e5265ad7f425
Anthony Tamburro <anthonytamburro89@gmail.com>
2019-10-26 22:21:30 -0400

    first hand in, need help

--- index.js
+++ index.js
@@ -8,39 +8,38 @@ $(function() {
      $(this).parent(this).parent().remove();
      alert('clicked delete');
   })
- }); 
+ 
+  $('.js-shopping-list-form').submit(function() {
+    $(this).preventDefault();
 
+    $('submit').click(function(){
 
-/* possible path for delete function...
-
-    $('ul').on('click', '.shopping-item-delete', function() {
-        $(this).closest("li").remove("li");
+    $('ul').append(
+    "<li> " 
+    <span class="shopping-item"> $(shopping-list-entry.val())</span> 
+    <div class="shopping-item-controls"> 
+      <button class="shopping-item-toggle"> 
+        <span class="button-label">check</span> 
+      </button> 
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button> 
+    </div> 
+    "</li>"
+    );
     })
-
-or 
-    $('.shopping-item-delete').click(function() {
-        this.closest('li').remove();
     })
-
-
-or I also tried this...
-
-    $('.shopping-item-delete').click(function() {
-      this.remove().parent('li');
   })
 
-
-*/
-
-
-/*
+ /*
 Beginning of my thoughts for adding other items to list...
 
 
 $(".submit").click(function(event) {
 event.preventDefault();
+
     $("ul").append(
-"<li>" + <span class="shopping-item">this.text(`${event.key}`)</span>
+"<li>" + <span class="shopping-item">`$(shopping-list-entry.val())`</span>
 <div class="shopping-item-controls">
   <button class="shopping-item-toggle">
     <span class="button-label">check</span>
@@ -51,21 +50,4 @@ event.preventDefault();
 </div> + "</li>"
 );
 })
-*/
-
-/*
- $('.shopping-item-toggle').click(function(){
-        console.log('this', this)
-      
-        $(this).toggleClass('shopping-item__checked');
-    
- }); 
-});*/
-
-/*
-$('.shopping-item-toggle').on('click',function(){
-
-    
-    $('.shopping-item').toggleClass('shopping-item__checked');
-    alert('script loaded');
 */
