f18904e055f063c3636b01f4e7d327053116d062
Anthony Tamburro <anthonytamburro89@gmail.com>
2019-11-02 12:56:22 -0400

    Attempt to submit form, check and delete work if you cut out form command

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-$(function() {/*
- $('.shopping-item-toggle').click(function(){
-    $(this).closest('.shopping-item').toggleClass('shopping-item__checked');
-    alert('script loaded');
- })*/
+$(function() {
+
+  $('js-shopping-list-form').on('submit', function () {
+    $(this).preventDefault();
+    alert('submit');
+    let addNewItem = $('shopping-list-entry').val();
+    $('ul').append('<li><span class="shopping-item"></span> 
+    <div class = "shopping-item-controls"> 
+      <button class="shopping-item-toggle"> 
+        <span class="button-label">check</span> 
+      </button> 
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button> 
+    </div> 
+    </li>');
+  
+  })
+
 
  $('ul').on('click', '.shopping-item-toggle', function() {
   $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
@@ -11,51 +25,4 @@ $(function() {/*
 $('ul').on('click', '.shopping-item-delete', function(){
   $(this).closest('li').remove();
 });
-/*
-    $('.shopping-item-delete').click(function() {
-     $(this).parent(this).parent().remove();
-     alert('clicked delete');
-  });
- 
-  $('.js-shopping-list-form').submit(function() {
-    $(this).preventDefault();
-
-    $('submit').click(function(){
-
-    $('ul').append(
-    "<li> " 
-    <span class="shopping-item"> $(shopping-list-entry.val())</span> 
-    <div class="shopping-item-controls"> 
-      <button class="shopping-item-toggle"> 
-        <span class="button-label">check</span> 
-      </button> 
-      <button class="shopping-item-delete">
-        <span class="button-label">delete</span>
-      </button> 
-    </div> 
-    "</li>"
-    );
-    });
-    })*/
-  })
-
- /*
-Beginning of my thoughts for adding other items to list...
-
-
-$(".submit").click(function(event) {
-event.preventDefault();
-
-    $("ul").append(
-"<li>" + <span class="shopping-item">`$(shopping-list-entry.val())`</span>
-<div class="shopping-item-controls">
-  <button class="shopping-item-toggle">
-    <span class="button-label">check</span>
-  </button>
-  <button class="shopping-item-delete">
-    <span class="button-label">delete</span>
-  </button>
-</div> + "</li>"
-);
 })
-*/
