2e6aeae6ab0f2b0ac3cc70a8440d7430906b09cc
Anthony Tamburro <anthonytamburro89@gmail.com>
2019-11-02 18:31:57 -0400

    Module 8.3 solution

--- index.js
+++ index.js
@@ -3,30 +3,22 @@ $(function() {
   $('body').find('form').submit(function (e) {
     alert('submit');
     e.preventDefault('submit');
+
+    const newItemText = $(this).find('#shopping-list-entry');
+
+$('ul').append("<li>"+
+"<span class= 'shopping-item'>"+`${newItemText.val()}`+
+"</span>" +
+"<div class = 'shopping-item-controls'>" +
+"<button class='shopping-item-toggle'>" + 
+"<span class='button-label'>check</span>" +
+"</button>" +
+"<button class='shopping-item-delete'>" +
+"<span class='button-label'>delete</span>" +
+"</button>"+
+"</div>"+ 
+"</li>");
   });
-
-/*
-$('form').on('click',$(this).closest(':button'), function () {
-        alert('submit');
-        $(this).preventDefault();
-  });
-  */
-
-
-/*
-      $('ul').append('<li><span class="shopping-item">$(('shopping-list-entry').val(''))</span> 
-    <div class = "shopping-item-controls">
-      <button class="shopping-item-toggle"> 
-        <span class="button-label">check</span> 
-      </button> 
-      <button class="shopping-item-delete">
-        <span class="button-label">delete</span>
-      </button> 
-    </div> 
-    </li>');
-  
-  })*/
-
 
  $('ul').on('click', '.shopping-item-toggle', function() {
   $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
